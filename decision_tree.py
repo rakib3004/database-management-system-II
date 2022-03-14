ad4522aa9fd11e7ba4b98da18b397289c52641a6
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-14 10:04:20 +0600

    write code for decision tree

---
+++ decision_tree.py
@@ -0,0 +1,175 @@
+from random import seed
+from random import randrange
+from csv import reader
+
+# Load a CSV file
+
+def load_csv(filename):
+    file = open(filename, "rt")
+    lines = reader(file)
+    dataset = list(lines)
+    return dataset
+
+# Convert string column to float
+
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+# Split a dataset into k folds
+
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+# Calculate accuracy percentage
+
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+# Evaluate an algorithm using a cross validation split
+
+def evaluate_algorithm(dataset, algorithm, n_folds):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list(fold)
+        predicted = algorithm(train_set, test_set)
+        actual = [row[feature_intdx] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
+# Split a dataset based on an attribute and an attribute value
+
+def test_split(index, value, dataset):
+    left, right = list(), list()
+    for row in dataset:
+        if row[index] < value:
+            left.append(row)
+        else:
+            right.append(row)
+    return left, right
+
+# Calculate the Gini index for a split dataset
+
+def gini_index(groups, classes):
+    # count all samples at split point
+    n_instances = float(sum([len(group) for group in groups]))
+    # sum weighted Gini index for each group
+    gini = 0.0
+    for group in groups:
+        size = float(len(group))
+        # avoid divide by zero
+        if size == 0:
+            continue
+        score = 0.0
+        # score the group based on the score for each class
+        for class_val in classes:
+            p = [row[feature_intdx] for row in group].count(class_val) / size
+            score += p * p
+        # weight the group score by its relative size
+        gini += (1.0 - score) * (size / n_instances)
+    return gini
+
+# Select the best split point for a dataset
+
+def get_split(dataset):
+    class_values = list(set(row[feature_intdx] for row in dataset))
+    b_index, b_value, b_score, b_groups = 9999999, 9999999, 9999999, None
+    for index in range(len(dataset[0])):
+        if index == feature_intdx:
+            continue
+        for row in dataset:
+            groups = test_split(index, row[index], dataset)
+            gini = gini_index(groups, class_values)
+            if gini < b_score:
+                b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+    return {'index': b_index, 'value': b_value, 'groups': b_groups}
+
+# Create a terminal node value
+
+def to_terminal(group):
+    return group[0][feature_intdx]
+
+# Create child splits for a node or make terminal
+
+def split(node):
+    left, right = node['groups']
+    del(node['groups'])
+    # check for a no split
+    if not left or not right:
+        node['left'] = node['right'] = to_terminal(left + right)
+        return
+
+    node['left'] = get_split(left)
+    split(node['left'])
+
+    node['right'] = get_split(right)
+    split(node['right'])
+
+# Build a decision tree
+
+def build_tree(train):
+    root = get_split(train)
+    split(root)
+    return root
+
+# Make a prediction with a decision tree
+
+def predict(node, row):
+    if row[node['index']] < node['value']:
+        if isinstance(node['left'], dict):
+            return predict(node['left'], row)
+        else:
+            return node['left']
+    else:
+        if isinstance(node['right'], dict):
+            return predict(node['right'], row)
+        else:
+            return node['right']
+
+# Classification and Regression Tree Algorithm
+
+def decision_tree(train, test):
+    tree = build_tree(train)
+    predictions = list()
+    for row in test:
+        prediction = predict(tree, row)
+        predictions.append(prediction)
+    return(predictions)
+
+seed(1)
+# load and prepare data
+filename = 'wine.csv'
+dataset = load_csv(filename)
+# convert string attributes to integers
+for i in range(len(dataset[0])-1):
+    str_column_to_float(dataset, i)
+
+# evaluate algorithm
+n_folds = 10
+## isolate 617
+#wine 13
+## letter recognization 16
+#ionsphier 34
+## parkinsons 21
+feature_intdx = 13
+scores = evaluate_algorithm(dataset, decision_tree, n_folds)
+print('Scores: %s' % scores)
+print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
