7ed33d1f43df39296154e460d4d7d548ddbc99c3
rakib3004 <bsse1129@iit.du.ac.bd>
2022-06-01 22:50:26 +0600

    intermidiate stage for wrap up

--- DataManupulationSystem.py
+++ DataManupulationSystem.py
@@ -1,14 +1,16 @@
   
-import json
-
 
 
 with open('Item.txt') as videoItemId:
-    lines = videoItemId.readlines()
-    lines=str(lines)
+    item_list = videoItemId.readlines()
+    print('---01----')
+    item_list=str(item_list)
+    print('---02----')
 
-    lines=lines.replace("\\n","").replace("\\r","")
-    print(lines)
+    item_list=item_list.replace("\\n","").replace("\\r","")
+    print('---03----')
+
+    print(item_list)
 
 videoItemId.close()
 
@@ -18,9 +20,14 @@ with open('UserRating.csv') as userRatingID:
     userRating=userRating.split('\n')
     for u_item in userRating:
         u_item=u_item.split(',')
+        user_index=u_item[0]
+        timestamp=u_item[1]
+        item_no=0
         for ux in u_item:
-            
-
+            item_no=item_no+1
+            ux=int(ux)
+            if(ux>0 and ux<6):
+                print(user_index,item_list[item_no],ux,timestamp)
    
 
 
