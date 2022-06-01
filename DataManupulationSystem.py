00893281d6efd556c05e02cf38df8b2a1d6edd1b
rakib3004 <bsse1129@iit.du.ac.bd>
2022-06-01 16:38:09 +0600

    add to complete authorization occupation

--- DataManupulationSystem.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-CSVData = open("UserRating.csv")
-Array2d_result = np.loadtxt(CSVData, delimiter=",")
-print(Array2d_result)
---
+++ VideoManupulation.py
@@ -0,0 +1,26 @@
+  
+import json
+
+
+
+def val_num(video_id):
+    val=0
+    for i in video_id:
+        val+=ord(i)
+
+    return val
+
+# Opening JSON file
+f = open('Video.json')
+  
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+  
+# Iterating through the json
+# list
+for i in data:
+    print(val_num(i['videoId']))
+  
+# Closing file
+f.close()
