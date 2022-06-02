2f21d2f962a70478095a0d537ef73e57312f3c57
rakib3004 <bsse1129@iit.du.ac.bd>
2022-06-02 08:02:59 +0600

    complete to making real user rating formate final

--- DataManupulationSystem.py
+++ DataManupulationSystem.py
@@ -1,33 +1,9 @@
-  
-  
-  
-  
-  
-  
-  
+
   
 item_list=[]
 with open('Item.txt') as videoItemId:
     item_list = videoItemId.readlines()
-    #item_list=str(item_list)
-    #item_list=item_list.replace("\\n","").replace("\\r","")
-    
-    
-    #print(item_list)
-    
-    '''
-    print('---01----')
-    
-    print('---02----')
-
-    item_list=item_list.replace("\\n","").replace("\\r","")
-    print('---03----')
-
-    print(item_list)
-    '''
-#videoItemId.close()
-
-
+   
 
 with open('UserRating.csv') as videoItemId:
     userRating=videoItemId.read()
@@ -37,10 +13,8 @@ with open('UserRating.csv') as videoItemId:
         user_index=u_item[0]
         timestamp=u_item[1]
         item_no=0
-        #print(user_index,'->',item_no)
         for ux in u_item:
             item_no=item_no+1
-            #print(item_no,end='-')
             ux=int(ux)
             item_index=str(item_list[item_no])
             item_index=item_index.replace("\n","").replace("\r","")
