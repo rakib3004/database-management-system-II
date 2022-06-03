8c9818fa431c23281d453e12aa0a80d6e5de682a
rakib3004 <bsse1129@iit.du.ac.bd>
2022-06-03 16:16:04 +0600

    add new testing data

--- AppliedMachineLearning/date.py
+++ AppliedMachineLearning/date.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import random
+
+from scipy import rand
 
 time='05/24/22 23:24:17'
 time=time.replace("\r","")
@@ -10,3 +13,5 @@ ts = datetime.timestamp(dt)
 print("Date and time is:", dt)
 print("Timestamp is:", int(ts))
 
+for i in range(100):
+    print(random.randint(0,60))
