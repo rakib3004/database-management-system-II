32df21f60ef9bad0f909e184a446d109f09d68eb
rakib3004 <bsse1129@iit.du.ac.bd>
2022-06-03 12:11:49 +0600

    formate date and time

---
+++ AppliedMachineLearning/date.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+time='05/24/22 23:24:17'
+time=time.replace("\r","")
+
+dt = datetime.strptime(time, '%m/%d/%y %H:%M:%S')
+
+ts = datetime.timestamp(dt)
+
+print("Date and time is:", dt)
+print("Timestamp is:", int(ts))
