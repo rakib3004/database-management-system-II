7febc57dd977a45d3ff1d91cf3ef172504d1cd72
rakib3004 <bsse1129@iit.du.ac.bd>
2022-06-05 19:53:22 +0600

    databased reformating

--- AppliedMachineLearning/ItemBasedRecommendation.py
+++ AppliedMachineLearning/ItemBasedRecommendation.py
@@ -1,3 +1,5 @@
+import random
+import sys
 import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
@@ -6,6 +8,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+userId=sys.argv[1]
+userId=int(userId)
+
+
+
 
 def dataFormatting(recommendedItems):
     recommendedItems=set(recommendedItems)
@@ -13,8 +20,10 @@ def dataFormatting(recommendedItems):
     itemIterator=0
     for r in recommendedItems:
         itemIterator=itemIterator+1
-        if(r>57):
-            recommendedItems.pop(r)
+        if(r>56):
+            recommendedItems.pop(itemIterator-1)
+        
+    random.shuffle(recommendedItems)
 
     return recommendedItems
 
@@ -22,7 +31,7 @@ def dataFormatting(recommendedItems):
 
 header = ['user_id', 'item_id', 'rating', 'timestamp']
 dataset = pd.read_csv('user.data', sep='\t', names=header)
-print(dataset.head())
+#print(dataset.head())
 
 
 n_users = dataset.user_id.unique().shape[0]
@@ -33,7 +42,7 @@ n_items = dataset['item_id'].max()
 A = np.zeros((n_users, n_items), dtype=int)
 for line in dataset.itertuples():
     A[line[1]-1, line[2]-1] = line[3]
-print("Original rating matrix : ", A)
+#print("Original rating matrix : ", A)
 
 for i in range(len(A)):
     for j in range(len(A[0])):
@@ -43,7 +52,6 @@ for i in range(len(A)):
             A[i][j] = 0
 
 csr_sample = csr_matrix(A)
-print(csr_sample)
 
 knn = NearestNeighbors(metric='cosine', algorithm='brute',
                        n_neighbors=3, n_jobs=-1)
@@ -53,17 +61,17 @@ dataset_sort_des = dataset.sort_values(
     ['user_id', 'timestamp'], ascending=[True, False])
 
 
-filter1 = dataset_sort_des[dataset_sort_des['user_id'] == 17].item_id
-filter1 = filter1.tolist()
+itemsLikedByUser = dataset_sort_des[dataset_sort_des['user_id'] == userId].item_id
+itemsLikedByUser = itemsLikedByUser.tolist()
 
 
 
-print("Items liked by user: ", filter1)
+#print("Items liked by user: ", itemsLikedByUser)
 
 
 distances1 = []
 recommendedItems = []
-for i in filter1:
+for i in itemsLikedByUser:
     distances, indices = knn.kneighbors(csr_sample[i], n_neighbors=3)
     indices = indices.flatten()
     indices = indices[1:]
@@ -75,9 +83,7 @@ for i in filter1:
 recommendedItems=dataFormatting(recommendedItems)
 print("Items to be recommended: ",recommendedItems)
 
-#print("Items to be recommended: ", recommendedItems)
-
-
+'''
 youTubeVideoUrlListPlainText = open("YouTubeData/YouTubeVideoID.txt", encoding="utf8")
 
 youTubeVideoUrlListFile=youTubeVideoUrlListPlainText.readlines()
@@ -99,9 +105,4 @@ for f in recommendedItems:
         continue
     #print(youTubeVideoUrlListFile[f], end='')
     print('{ "videoID":"'+youTubeVideoUrlListFile[f].strip()+'"'+', "videoTitle":"'+youTubeVideoUrlTitleFile[f].strip()+'" , "videoTopic":"'+youTubeVideoUrlTopicFile[f].strip()+'" },')
-
-'''
-    print('/"videoID"/:/"+'youTubeVideoUrlListFile[f], end='')
-
-
 '''
